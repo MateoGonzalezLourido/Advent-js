32fe9c470ac55aff5564b82cdeb192620ce0702d
MateoGonzalezLourido <128542673+MateoGonzalezLourido@users.noreply.github.com>
2024-03-27 21:36:49 +0100

    Mejora reto 1

--- Primer Regalo Repetido/script.js
+++ Primer Regalo Repetido/script.js
@@ -1,40 +1,13 @@
 //codigo para el reto js->  https://adventjs.dev/es/challenges/2023/1
 function findFirstRepeated(gifts) {
-    let n_repetidos = []
-    gifts.forEach((numeroMirar) => {
-        if (numeroMirar != "") {
-            const indices = []
-            for (let j = 0; j < gifts.length; j++) {
-                if (numeroMirar == gifts[j]) {
-                    indices.push(j)
-                    gifts[j] = ""//vacio
-                }
-            }
-            if (indices.length > 1) {//se repite
-                n_repetidos.push([numeroMirar, indices])
-            }
+    const regalos = []
+    for (let i = 0; i < gifts.length; i++) {
+        if (regalos.includes(gifts[i]) === true) {
+            return gifts[i]
         }
-    })
-    if (n_repetidos.length == 0) {//no hay repetidos
-        return -1
+        regalos.push(gifts[i])
     }
-    else if (n_repetidos.length == 1) {//solo hay un repetido
-        return n_repetidos[0][0]
-    }
-    else {//mas de un repetido
-        const indiceSegundaAparicion = []
-        n_repetidos.forEach((item) => {
-            indiceSegundaAparicion.push(item[1][1])
-        })
-        let indiceMenor = 0
-        for (let i = 0; i < indiceSegundaAparicion.length - 1; i++) {
-            if (indiceSegundaAparicion[i] > indiceSegundaAparicion[i + 1]) {
-                indiceMenor = i + 1
-            }
-        }
-        //devolver numero cuya segunda ocurrencia aparezca primero en la lista
-        return n_repetidos[indiceMenor][0]
-    }
+    return -1
 }
 const giftIds = [5, 1, 5, 1]
 console.log(findFirstRepeated(giftIds))
