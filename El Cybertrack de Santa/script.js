946a8a14251ede627b9da7fece5e68f7d6f7c221
MateoGonzalezLourido <128542673+MateoGonzalezLourido@users.noreply.github.com>
2024-03-27 22:22:37 +0100

    mejora codigo reto 5

--- El Cybertrack de Santa/script.js
+++ El Cybertrack de Santa/script.js
@@ -1,29 +1,27 @@
 //codigo del reto js -> https://adventjs.dev/es/challenges/2023/5
 function cyberReindeer(road, time) {
+    const procesos = [road]
     let procesoActual = road
-    let procesos = [road]
+
     for (let i = 1; i < time; i++) {
-        if (i == 5) {
-            procesoActual = procesoActual.replaceAll('|', '*')//abrir barreras
+        if (i === 5) {
+            procesoActual = procesoActual.replaceAll('|', '*')
         }
-        let camino = procesoActual.split('')
-        for (let i = 0; i < camino.length; i++) {
-            if (camino[i] == 'S') {//trineo
-                if (camino[i + 1] != '|') {//moverlo
-                    camino[i + 1] = 'S'
-                    if (road[i] == '|') {
-                        camino[i] = '*'
-                    }
-                    else {
-                        camino[i] = '.'
-                    }
-                }
-                break
+        procesoActual = procesoActual.split('')
+        const paso = procesoActual.indexOf('S')
+        if (procesoActual[paso + 1] !== '|') {
+            procesoActual[paso + 1] = 'S'
+            if (road[paso] === '|') {
+                procesoActual[paso] = '*'
+            }
+            else {
+                procesoActual[paso] = '.'
             }
         }
-        procesoActual = (camino.toString()).replaceAll(',', '')
+        procesoActual = procesoActual.join('')
         procesos.push(procesoActual)
     }
+
     return procesos
 }
 console.log(cyberReindeer('S..|...|..', 10))
