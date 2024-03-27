82097d9d00ab433b72fe62e160a3f0b5d0354b37
MateoGonzalezLourido <128542673+MateoGonzalezLourido@users.noreply.github.com>
2024-03-27 22:04:49 +0100

    mejora codigo reto 4

--- Dale la vuelta a los parentesis/script.js
+++ Dale la vuelta a los parentesis/script.js
@@ -1,45 +1,20 @@
+//codigo para el reto js-> https://adventjs.dev/es/challenges/2023/4
 function decode(message) {
+    const stack = []
     let texto = ""
-    let parentesis = false
+
     for (let i = 0; i < message.length; i++) {
-        if (!parentesis) {
-            if (message[i] != "(") {
-                texto += message[i]
-            }
-            else {
-                parentesis = true
-            }
-        }
-        else {
-            let textoVolteado = ""
-            for (let j = i; j < message.length; j++) {
-                if (message[j] != "(") {
-                    if (message[j] != ")") {
-                        textoVolteado = message[j] + textoVolteado
-                    }
-                    else {
-                        texto += textoVolteado
-                        i = j
-                        parentesis = false
-                        break
-                    }
-                }
-                else {
-                    let parteTexto = ""
-                    for (let k = j + 1; k < message.length; k++) {
-                        if (message[k] != ")") {
-                            parteTexto += message[k]
-                        }
-                        else {
-                            j = k
-                            textoVolteado = parteTexto + textoVolteado
-                            break
-                        }
-                    }
-                }
-            }
+        if (message[i] === "(") {
+            stack.push(texto)
+            texto = ""
+        } else if (message[i] === ")") {
+            const reversedText = texto.split("").reverse().join("")
+            texto = stack.pop() + reversedText
+        } else {
+            texto += message[i]
         }
     }
+
     return texto
 }
 console.log(decode('sa(u(cla)atn)s'))
