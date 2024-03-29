c887baace8690153a4feb73cbe5e0e8587ac694a
MateoGonzalezLourido <128542673+MateoGonzalezLourido@users.noreply.github.com>
2024-03-29 18:44:56 +0100

    mejora codigo reto 16

--- Despliegue en viernes/script.js
+++ Despliegue en viernes/script.js
@@ -3,26 +3,27 @@ function transformTree(tree) {
         value,
         left: null,
         right: null
-    });
+    })
     if (tree.length === 0) {
-        return null;
+        return null
     }
 
-    const nodes = tree.map(val => (val !== null ? arbolValor(val) : null));
+    const nodes = tree.map(val => (val !== null ? arbolValor(val) : null))
 
     for (let i = 0; i < nodes.length; i++) {
         if (nodes[i] !== null) {
-            const leftChildIndex = 2 * i + 1;
-            const rightChildIndex = 2 * i + 2;
+            const leftChildIndex = 2 * i + 1
+            const rightChildIndex = 2 * i + 2
 
             if (leftChildIndex < nodes.length) {
-                nodes[i].left = nodes[leftChildIndex];
+                nodes[i].left = nodes[leftChildIndex]
             }
             if (rightChildIndex < nodes.length) {
-                nodes[i].right = nodes[rightChildIndex];
+                nodes[i].right = nodes[rightChildIndex]
             }
         }
     }
 
-    return nodes[0];
+    return nodes[0]
 }
+console.log(transformTree([1, 2, 3, 4, 5, 6, 7]))
